3e9ecbad44509252f66d37ec8ce5e6fae4855d08
Bart-Jan van Strien <bartjanvs@gmail.com>
2020-10-21 16:23:37 +0200

    Some extra comments.

--- opdrachten/opdracht4/ncurses.cpp
+++ opdrachten/opdracht4/ncurses.cpp
@@ -1,4 +1,8 @@
+// Embedded Systems | HvA 2020
+// Opdracht 4 week 2
 //
+// Rowan Bolding
+// Bart-Jan van Strien
 //
 // Compile: g++ -Wall -o ncurses ncurses.cpp -lncurses
 // Run    : ./ncurses
@@ -10,19 +14,21 @@
 int main()
 {
     WINDOW * mainwin, * childwin;
+
+    // init main window
     int width = 30, height = 6;
     int rows = 25, cols = 80;
     int x = (cols - width) /2;
     int y = (rows - height) /2;
-
-    // init main window
     if ( (mainwin = initscr()) == NULL )
     {
         fprintf(stderr, "ERROR INIT NCURSES\n");
         exit(EXIT_FAILURE);
     }
+
     noecho();
     keypad(mainwin, TRUE);
+
     // init child window
     childwin = subwin(mainwin, height, width, y, x);
     box(childwin, '|', '-');
@@ -30,14 +36,15 @@ int main()
     mvwaddstr(childwin, 2, 11, "HvA 2020");
     mvwaddstr(childwin, 3, 8, "Rowan Bolding");
     mvwaddstr(childwin, 4, 5, "Bart-Jan van Strien");
+    
     refresh();
+    // wait for user input before exit
     getch();
 	
+    // EXIT!
     delwin(childwin);
     delwin(mainwin);
     endwin();
     refresh();
-
     return EXIT_SUCCESS;
-   
 }
